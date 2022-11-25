4a04ad0aabb0f3ea0af949d31e7224849b9a546e
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-25 16:03:40 +0530

    fourth

--- Gambler.java
+++ Gambler.java
@@ -8,24 +8,36 @@ public class Gambler {
 		int betAmount = 1;
 		int winCash = stakeAmount + stakeAmount/2;
 		int lostCash = stakeAmount - stakeAmount/2;
-		int cash = stakeAmount;
 
+		int days;
+		int totalcash = 0;
 
 		System.out.println("Gambler's stake amount : "+stakeAmount+"$");
 		System.out.println("Gambler bets for each game : "+betAmount+"$");
 
-		while ((cash != winCash) && (cash != lostCash)) {
+		for (days = 1; days <= 20; days++) {
+			int cash = stakeAmount;
+			while ((cash != winCash) && (cash != lostCash)) {
 
-			double chance =  Math.random();
-			if (chance > 0.5) {
-				System.out.println("Gambler won");
-				cash++;
-				System.out.println("current cash : "+cash);
-			}else {
-				System.out.println("Gambler lost");
-				cash--;
-				System.out.println("current cash : "+cash);
+				double chance =  Math.random();
+				if (chance > 0.5) {
+					System.out.println("Gambler won");
+					cash++;
+					System.out.println("current cash : "+cash);
+				}else {
+					System.out.println("Gambler lost");
+					cash--;
+					System.out.println("current cash : "+cash);
+				}
 			}
-		}
+
+			cash = cash - stakeAmount ;
+			totalcash = totalcash+cash;
+			if (totalcash > 0) {
+				System.out.println("Total cash won after 20 days :"+totalcash);
+			}else {
+				System.out.println("Total cash lost after 20 days : "+totalcash);
+			}
+		}	
 	}
 }
