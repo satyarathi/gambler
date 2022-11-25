610c88e569d5d3461f3f278ee898811c19aaa6fa
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-25 15:19:23 +0530

    third

--- Gambler.java
+++ Gambler.java
@@ -1,22 +1,31 @@
 package com.workshop;
 
 public class Gambler {
-	
+
 	public static void main(String[] args) {
 
 		int stakeAmount = 100;
 		int betAmount = 1;
-		int win;
-		int loose;
-		
+		int winCash = stakeAmount + stakeAmount/2;
+		int lostCash = stakeAmount - stakeAmount/2;
+		int cash = stakeAmount;
+
+
 		System.out.println("Gambler's stake amount : "+stakeAmount+"$");
 		System.out.println("Gambler bets for each game : "+betAmount+"$");
-		
-		double chance =  Math.random();
-		if (chance > 0.5) {
-			System.out.println("Gambler won");
-		}else {
-			System.out.println("Gambler lost");
+
+		while ((cash != winCash) && (cash != lostCash)) {
+
+			double chance =  Math.random();
+			if (chance > 0.5) {
+				System.out.println("Gambler won");
+				cash++;
+				System.out.println("current cash : "+cash);
+			}else {
+				System.out.println("Gambler lost");
+				cash--;
+				System.out.println("current cash : "+cash);
+			}
 		}
 	}
 }
