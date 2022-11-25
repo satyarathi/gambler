bf96f1db16e14d4813434893e9a52797845c6d6d
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-25 14:23:02 +0530

    gambler

---
+++ Gambler.java
@@ -0,0 +1,13 @@
+package com.workshop;
+
+public class Gambler {
+	
+	public static void main(String[] args) {
+
+		int stakeAmount = 100;
+		int betAmount = 1;
+		System.out.println("Gambler stake amount : "+stakeAmount+"$");
+		System.out.println("Gambler betting for each game : "+betAmount+"$");
+		
+	}
+}
