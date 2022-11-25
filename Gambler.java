81f10264c060376403e3a3e635533b95e723900f
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-25 14:52:46 +0530

    second

--- Gambler.java
+++ Gambler.java
@@ -6,9 +6,17 @@ public class Gambler {
 
 		int stakeAmount = 100;
 		int betAmount = 1;
+		int win;
+		int loose;
 		
-		System.out.println("Gambler stake amount : "+stakeAmount+"$");
+		System.out.println("Gambler's stake amount : "+stakeAmount+"$");
 		System.out.println("Gambler bets for each game : "+betAmount+"$");
 		
+		double chance =  Math.random();
+		if (chance > 0.5) {
+			System.out.println("Gambler won");
+		}else {
+			System.out.println("Gambler lost");
+		}
 	}
 }
